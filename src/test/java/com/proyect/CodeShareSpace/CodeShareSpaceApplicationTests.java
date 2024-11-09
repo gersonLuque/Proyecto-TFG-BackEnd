bb95b46f24aedcd91b10abdfe8710f341756d21f
Gerson Luque <gerson126@hotmail.com>
2024-11-09 11:50:27 +0100

    Commit inicial

---
+++ src/test/java/com/proyect/CodeShareSpace/CodeShareSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.CodeShareSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeShareSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
