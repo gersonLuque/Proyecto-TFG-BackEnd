2a848af5beac96ff7d4c544df6839fbdd74daf88
Gerson Luque <gerson126@hotmail.com>
2024-11-09 11:56:18 +0100

    Agregado modelo de usuario junto con los roles

--- src/main/java/com/proyect/CodeShareSpace/model/User.java
+++ src/main/java/com/proyect/CodeShareSpace/model/User.java
@@ -1,13 +1,24 @@
 package com.proyect.CodeShareSpace.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String nameComplete;
 
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
 }
