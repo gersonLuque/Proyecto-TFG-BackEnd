0cd7356c1e15bfd0dde7cd059da883c4396b145e
Gerson Luque <gerson126@hotmail.com>
2024-11-16 15:39:04 +0100

    feat: agregado y testado el controllador UserControler

--- src/main/java/com/proyect/CodeShareSpace/dto/UserDto.java
+++ src/main/java/com/proyect/CodeShareSpace/dto/UserDto.java
@@ -2,15 +2,13 @@ package com.proyect.CodeShareSpace.dto;
 
 import com.proyect.CodeShareSpace.persistence.model.Course;
 import com.proyect.CodeShareSpace.persistence.model.Rol;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
 public class UserDto {
     private Long userId;
     private String username;
@@ -18,4 +16,72 @@ public class UserDto {
     private String completeName;
     private Rol rol;
     private Set<Course> courses;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCompleteName() {
+        return completeName;
+    }
+
+    public void setCompleteName(String completeName) {
+        this.completeName = completeName;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+        UserDto userDto = (UserDto) object;
+        return Objects.equals(userId, userDto.userId) && Objects.equals(username, userDto.username) && Objects.equals(password, userDto.password) && Objects.equals(completeName, userDto.completeName) && rol == userDto.rol && Objects.equals(courses, userDto.courses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, password, completeName, rol, courses);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" + "userId=" + userId + ", username='" + username + '\'' + ", password='" + password + '\'' + ", completeName='" + completeName + '\'' + ", rol=" + rol + ", courses=" + courses + '}';
+    }
 }
---
+++ src/test/java/com/proyect/CodeShareSpace/UserMapperTest.java
@@ -0,0 +1,41 @@
+package com.proyect.CodeShareSpace;
+
+import com.proyect.CodeShareSpace.dto.UserDto;
+import com.proyect.CodeShareSpace.mapper.UserMapper;
+import com.proyect.CodeShareSpace.persistence.model.Rol;
+import com.proyect.CodeShareSpace.persistence.model.User;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserMapperTest {
+    private UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+
+    @Test
+    public void testUserToUserDto() {
+        // Crear un objeto User para probar
+        User user = new User();
+        user.setUserId(1L);
+        user.setUsername("juanperez");
+        user.setPassword("password123");
+        user.setCompleteName("Juan Pérez");
+        user.setRol(Rol.STUDENT);
+        user.setCourses(new HashSet<>());  // Puedes agregar cursos si es necesario
+
+        // Mapear el User a UserDto
+        UserDto userDto = userMapper.userToUserDto(user);
+
+        // Verificar que el mapeo es correcto
+        assertNotNull(userDto);
+        assertEquals(user.getUserId(), userDto.getUserId());
+        assertEquals(user.getUsername(), userDto.getUsername());
+        assertEquals(user.getPassword(), userDto.getPassword());
+        assertEquals(user.getCompleteName(), userDto.getCompleteName());
+        assertEquals(user.getRol(), userDto.getRol());
+        assertEquals(user.getCourses().size(), userDto.getCourses().size());
+    }
+}
