a87be8ad3bc1e811c4f5efe80277cabc8a6258cf
Gerson Luque <gerson126@hotmail.com>
2024-11-16 18:29:36 +0100

    fix: arreglado el bucle infinito con UserDto

--- src/main/java/com/proyect/CodeShareSpace/mapper/ICourseMapper.java
+++ src/main/java/com/proyect/CodeShareSpace/mapper/ICourseMapper.java
@@ -1,15 +1,19 @@
 package com.proyect.CodeShareSpace.mapper;
 
 import com.proyect.CodeShareSpace.dto.CourseDto;
+import com.proyect.CodeShareSpace.dto.UserDto;
 import com.proyect.CodeShareSpace.persistence.model.Course;
+import com.proyect.CodeShareSpace.persistence.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper
 public interface ICourseMapper {
     ICourseMapper INSTANCE = Mappers.getMapper(ICourseMapper.class);
 
-    CourseDto courseToCourseDto(Course course);
-    Course courseDtoToCourse(CourseDto courseDto);
 
 }
