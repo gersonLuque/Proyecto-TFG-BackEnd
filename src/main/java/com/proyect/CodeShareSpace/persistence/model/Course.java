e838b2a04bc9b822335d2683ee04e7f297c844df
Gerson Luque <gerson126@hotmail.com>
2024-11-16 16:14:37 +0100

    test: agregados los tests para el mapper ICourseMapper

--- src/main/java/com/proyect/CodeShareSpace/persistence/model/Course.java
+++ src/main/java/com/proyect/CodeShareSpace/persistence/model/Course.java
@@ -1,13 +1,18 @@
 package com.proyect.CodeShareSpace.persistence.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "courses")
 public class Course {
 
