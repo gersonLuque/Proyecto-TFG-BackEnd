22e8f7fa0e1bccd637d48dc81c585bba696ed9f4
Gerson Luque <gerson126@hotmail.com>
2025-01-02 14:07:43 +0100

    feat: agregada la implementacion TaskServiceImpl

--- src/main/java/com/proyect/CodeShareSpace/repository/TaskRepository.java
+++ src/main/java/com/proyect/CodeShareSpace/repository/TaskRepository.java
@@ -1,8 +1,8 @@
 package com.proyect.CodeShareSpace.repository;
 
+import com.proyect.CodeShareSpace.persistence.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.config.Task;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
-    
+
 }
