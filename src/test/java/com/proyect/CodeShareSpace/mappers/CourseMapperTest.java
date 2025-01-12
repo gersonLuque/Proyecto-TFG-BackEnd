b71aebb975f5fd0f41e8d339579de6f8e3ca31ec
Javier del Cerro <jdelcerrodenava@gmail.com>
2025-01-12 19:03:07 +0100

    Implementa nuevo método test de CourseMapperTest.java

---
+++ src/test/java/com/proyect/CodeShareSpace/mappers/CourseMapperTest.java
@@ -0,0 +1,60 @@
+package com.proyect.CodeShareSpace.mappers;
+
+import java.util.List;
+
+import com.proyect.CodeShareSpace.DataProvider;
+import org.mapstruct.factory.Mappers;
+import org.junit.jupiter.api.Test;
+import com.proyect.CodeShareSpace.dto.CourseDto;
+import com.proyect.CodeShareSpace.mapper.ICourseMapper;
+import com.proyect.CodeShareSpace.persistence.model.Course;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CourseMapperTest {
+
+    private ICourseMapper iCourseMapper = Mappers.getMapper(ICourseMapper.class);
+
+    @Test
+    public void testCourseToCourseDto() {
+        // Given
+        Course course = DataProvider.newCourseMock();
+
+        // Mapea el Course -> CourseDto
+        CourseDto courseDto = iCourseMapper.courseToCourseDto(course);
+
+        // Then
+        assertNotNull(courseDto, "El CourseDto no debe ser null");
+        assertEquals(course.getCourseId(),courseDto.getCourseId());
+        assertEquals(course.getName(), courseDto.getName());
+    }
+
+    @Test
+    public void testCourseDtoToCourse() {
+        // Given
+        CourseDto courseDto = DataProvider.newCourseDTOMock();
+
+        // Mapea el CourseDto -> Course
+        Course course = iCourseMapper.courseDtoToCourse(courseDto);
+
+        // Then
+        assertNotNull(course, "El Course no debe ser null");
+        assertEquals(courseDto.getCourseId(), course.getCourseId());
+        assertEquals(courseDto.getName(), course.getName());
+    }
+
+    @Test
+    public void testCoursesToCourseDtos(){
+        // Given
+        List<Course> courseList = DataProvider.newCoursesListMock();
+
+        // Mapea una lista de Course -> CourseDto
+        List<CourseDto> courseDtoList = iCourseMapper.coursesToCourseDtos(courseList);
+
+        // Then
+        assertNotNull(courseDtoList);
+        assertEquals(courseList.size(), courseDtoList.size());
+        assertEquals(courseList.get(2).getCourseId(), courseDtoList.get(2).getCourseId());
+        assertEquals(courseList.get(1).getName(), courseDtoList.get(1).getName());
+    }
+
+}
