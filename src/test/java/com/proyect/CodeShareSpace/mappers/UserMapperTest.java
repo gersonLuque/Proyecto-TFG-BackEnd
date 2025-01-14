74279a10da4864e0748c0d232469c8cca6aa11f1
Javier del Cerro <jdelcerrodenava@gmail.com>
2025-01-14 18:29:33 +0100

    Implementa los 2 métodos test de UserMapper: -testUserToUserDto() -testUserDtoToUser()

--- src/test/java/com/proyect/CodeShareSpace/mappers/UserMapperTest.java
+++ src/test/java/com/proyect/CodeShareSpace/mappers/UserMapperTest.java
@@ -3,66 +3,59 @@ package com.proyect.CodeShareSpace.mappers;
 import com.proyect.CodeShareSpace.DataProvider;
 import com.proyect.CodeShareSpace.dto.UserDto;
 import com.proyect.CodeShareSpace.mapper.IUserMapper;
-import com.proyect.CodeShareSpace.persistence.model.Rol;
 import com.proyect.CodeShareSpace.persistence.model.User;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
+import org.mapstruct.factory.Mappers;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 
-@ExtendWith(MockitoExtension.class)
 public class UserMapperTest {
 
-    @Mock
-    private IUserMapper iUserMapper;
+    private IUserMapper iUserMapper = Mappers.getMapper(IUserMapper.class);
+
     @Test
     public void testUserToUserDto() {
         // Given
         User user = DataProvider.newUserMock();
-        UserDto expectedUserDto = DataProvider.newUserDtoMock(); // Guarda el DTO esperado para comparar
 
-        // When
-        Mockito.when(iUserMapper.userToUserDto(any(User.class))).thenReturn(expectedUserDto);
-        UserDto UserDto = iUserMapper.userToUserDto(user);
+        // When - se mapea User -> UserDto
+        UserDto userDto = iUserMapper.userToUserDto(user);
 
         // Then
-        Mockito.verify(iUserMapper).userToUserDto(user); // Verifica con la instancia real de User
-        assertNotNull(UserDto);
-        assertEquals(expectedUserDto, UserDto); // Compara objetos usando equals() (si está implementado) o compara campo por campo si no
-
-        assertEquals(expectedUserDto.getUserId(), UserDto.getUserId());
-        assertEquals(expectedUserDto.getUsername(), UserDto.getUsername());
-        assertEquals(expectedUserDto.getPassword(), UserDto.getPassword());
-        assertEquals(expectedUserDto.getCompleteName(), UserDto.getCompleteName());
-        assertEquals(expectedUserDto.getRol(), UserDto.getRol());
-        assertEquals(expectedUserDto.getCourses().size(), UserDto.getCourses().size());
+        assertNotNull(userDto);
+        assertEquals(userDto.getUserId(), user.getUserId());
+        assertEquals(userDto.getRol(), user.getRol());
+        assertEquals(userDto.getPassword(), user.getPassword());
+        assertEquals(userDto.getCompleteName(), user.getCompleteName());
+        assertEquals(userDto.getCourses(), user.getCourses());
+        assertEquals(userDto.getCourses(), user.getCourses());
     }
 
     @Test
     public void testUserDtoToUser() {
         // Given
         UserDto userDto = DataProvider.newUserDtoMock(); // DTO de entrada
-        User expectedUser = DataProvider.newUserMock(); // Entidad esperada
 
-        // When
-        Mockito.when(iUserMapper.userDtoToUser(any(UserDto.class))).thenReturn(expectedUser);
+        // When - se mapea UserDto -> User
         User user = iUserMapper.userDtoToUser(userDto);
 
         // Then
-        Mockito.verify(iUserMapper).userDtoToUser(any(UserDto.class)); // Verifica que se llamó al método
-        assertNotNull(user, "El objeto User no debería ser nulo");
-        assertInstanceOf(User.class, user, "El objeto debería ser de tipo User");
-        assertEquals(1L, user.getUserId(), "El UserId debería ser 1L");
-        assertEquals("anaFrank08", user.getUsername(), "El username debería ser 'anaFrank08'");
-        assertEquals("cacahuete", user.getPassword(), "El password debería ser 'cacahuete'");
-        assertEquals("Ana Cisneros", user.getCompleteName(), "El nombre completo debería ser 'Ana Cisneros'");
-        assertEquals(Rol.TEACHER, user.getRol(), "El rol debería ser TEACHER");
-        assertTrue(user.getCourses().isEmpty(), "La lista de cursos debería estar vacía");
+        assertNotNull(user);
+        assertEquals(user.getUserId(), userDto.getUserId());
+        assertEquals(user.getRol(), userDto.getRol());
+        assertEquals(user.getPassword(), userDto.getPassword());
+        assertEquals(user.getCompleteName(), userDto.getCompleteName());
+        assertEquals(user.getCourses(), userDto.getCourses());
+        assertEquals(user.getCourses(), userDto.getCourses());
+    }
+
+    @Test
+    public void testMapToUserBasicDto(){
+        // TODO
+    }
+
+    @Test
+    public void testUsersToUsersDto(){
+        // TODO
     }
 
 }
