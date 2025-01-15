9df7d1ab316bcac6cf98c4d36abe3fb1927cf192
Gerson Luque <gerson126@hotmail.com>
2025-01-15 20:59:37 +0100

    feat: se crea el endpoint /api/auth/logout para revokar el token

--- src/main/java/com/proyect/CodeShareSpace/controller/AuthController.java
+++ src/main/java/com/proyect/CodeShareSpace/controller/AuthController.java
@@ -2,13 +2,12 @@ package com.proyect.CodeShareSpace.controller;
 
 import com.proyect.CodeShareSpace.dto.user.LoginRequest;
 import com.proyect.CodeShareSpace.dto.user.LoginResponse;
+import com.proyect.CodeShareSpace.security.JwtService;
 import com.proyect.CodeShareSpace.service.interfaces.IAuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
@@ -17,8 +16,16 @@ public class AuthController {
     @Autowired
     private IAuthService iAuthService;
 
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("login")
     public ResponseEntity<LoginResponse> singIn(@RequestBody LoginRequest loginRequest){
         return ResponseEntity.ok(iAuthService.singIn(loginRequest));
+    }
+    @GetMapping("logout")
+    public ResponseEntity<Void> singOut(HttpServletRequest request){
+        iAuthService.singOut(jwtService.getTokenFromRequest(request));
+        return ResponseEntity.ok().build();
     }
 }
