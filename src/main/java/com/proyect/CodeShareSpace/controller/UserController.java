fe5dea7d965921e565cc0f9fdb4378e3863dd959
Gerson Luque <gerson126@hotmail.com>
2025-01-31 13:41:02 +0100

    docs: agregada la documentacion para el userController

--- src/main/java/com/proyect/CodeShareSpace/controller/UserController.java
+++ src/main/java/com/proyect/CodeShareSpace/controller/UserController.java
@@ -3,6 +3,8 @@ package com.proyect.CodeShareSpace.controller;
 import com.proyect.CodeShareSpace.dto.user.UserCreateDto;
 import com.proyect.CodeShareSpace.dto.user.UserDto;
 import com.proyect.CodeShareSpace.service.interfaces.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,26 +20,50 @@ public class UserController {
     @Autowired
     private IUserService iUserService;
 
+    @Operation(
+            summary = "Obtener todos los usuarios",
+            description = "Obtiene una lista de todos los usuarios registrados en el sistema. **Roles requeridos: TEACHER**",
+            security = {
+                    @SecurityRequirement(name = "bearerAuth")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<UserDto>> findAll(){
         return new ResponseEntity<>(iUserService.findAll(),HttpStatus.OK);
     }
 
-    @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Obtener un usuario por ID",
+            description = "Obtiene los detalles de un usuario según su ID. **Roles requeridos: STUDENT,TEACHER**",
+            security = {
+                    @SecurityRequirement(name = "bearerAuth")
+            }
+    )
+    @GetMapping("{userId}")
     public ResponseEntity<UserDto> findById(@PathVariable Long userId){
         return new ResponseEntity<>(iUserService.findById(userId), HttpStatus.OK);
     }
-    @PostMapping("prueba")
-    public ResponseEntity<String> create(){
-        return ResponseEntity.ok("estoy creando un usuario");
-    }
 
+    @Operation(
+            summary = "Crear un nuevo usuario",
+            description = "Crea un nuevo usuario utilizando los datos proporcionados en el objeto **UserCreateDto**. **Roles requeridos: TEACHER**",
+            security = {
+                    @SecurityRequirement(name = "bearerAuth")
+            }
+    )
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserCreateDto userCreateDto){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(iUserService.createUser(userCreateDto));
     }
 
+    @Operation(
+            summary = "Actualizar un usuario por Id",
+            description = "Actualiza un usuario utilizando los datos del objeto ** UserDto . Roles requeridos: TEACHER, STUDENT**",
+            security = {
+                    @SecurityRequirement(name = "bearerAuth")
+            }
+    )
     @PutMapping
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto){
         return ResponseEntity.status(HttpStatus.OK)
